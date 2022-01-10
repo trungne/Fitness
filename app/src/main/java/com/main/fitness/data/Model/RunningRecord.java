15a326878ae97387acaf48b570da0264e7fd903a
Tadaki741 <94947002+Tadaki741@users.noreply.github.com>
2022-01-10 17:20:59 +0700

    Update Running Record class
    + Add more field data and constructor

--- app/src/main/java/com/main/fitness/data/Model/RunningRecord.java
+++ app/src/main/java/com/main/fitness/data/Model/RunningRecord.java
@@ -1,8 +1,52 @@
 package com.main.fitness.data.Model;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class RunningRecord {
-    private String name;
-    private LocalDateTime time;
+    private String time;
+    private int step;
+    private Duration duration;
+    private String finishTime;
+    private int totalDistance;
+    private boolean isTrackCompleted;
+    private Double travelledDistance;
+
+    public RunningRecord(String time, int step, Duration duration, String finishTime, int totalDistance, boolean isTrackCompleted, Double travelledDistance) {
+        this.time = time;
+        this.step = step;
+        this.duration = duration;
+        this.finishTime = finishTime;
+        this.totalDistance = totalDistance;
+        this.isTrackCompleted = isTrackCompleted;
+        this.travelledDistance = travelledDistance;
+    }
+
+    public int getTotalDistance() {
+        return totalDistance;
+    }
+
+    public boolean isTrackCompleted() {
+        return isTrackCompleted;
+    }
+
+    public Double getTravelledDistance() {
+        return travelledDistance;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public String getFinishTime() {
+        return finishTime;
+    }
 }
