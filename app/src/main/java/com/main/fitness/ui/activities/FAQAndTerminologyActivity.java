ea48303482e94328eda534565e55c9b4737735bc
s3680599Huy <info.huytran99@gmail.com>
2022-01-11 09:41:25 +0700

    FAQ page fill

--- app/src/main/java/com/main/fitness/ui/activities/FAQAndTerminologyActivity.java
+++ app/src/main/java/com/main/fitness/ui/activities/FAQAndTerminologyActivity.java
@@ -3,14 +3,25 @@ package com.main.fitness.ui.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.main.fitness.R;
 
 public class FAQAndTerminologyActivity extends AppCompatActivity {
 
+    ImageButton backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq_and_terminology);
+
+        backButton = findViewById(R.id.activityFAQBackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
